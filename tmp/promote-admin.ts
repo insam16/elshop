72eba7614a8d2dee84bfe38bb61f07bc2d4ec3ed
yuna <yumin5541@naver.com>
2026-04-19 01:01:03 +0900

    promote

--- tmp/promote-admin.ts
+++ tmp/promote-admin.ts
@@ -17,7 +17,7 @@ async function main() {
       data: { role: "ADMIN" },
     });
 
-    console.log(`Success: User "${user.nickname}" (${user.email}) has been promoted to ADMIN.`);
+    console.log(`Success: User "${user.nickname}" (${user.id}) has been promoted to ADMIN.`);
   } catch (error) {
     console.error("Error: Could not find or update user with nickname:", nickname);
     process.exit(1);
